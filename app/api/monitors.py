0c4b8531b881c30d498ad8e18ba15dc648685265
anton <savenkovanton12@gmail.com>
2026-09-22 16:09:33 +0300

    end stage2

--- app/api/monitors.py
+++ app/api/monitors.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.database import get_db
 from app.schemas.monitor import MonitorResponse, MonitorCreate
-from app.db.crud import create_monitor
+from app.db.crud import create_monitor, get_monitors, get_monitor_by_id, delete_monitor
 
 router = APIRouter(prefix="/monitors", tags=["Monitors"])
 
@@ -10,3 +10,23 @@ router = APIRouter(prefix="/monitors", tags=["Monitors"])
 async def add_monitor(monitor_input: MonitorCreate, db: AsyncSession = Depends(get_db)):
     monitor = await create_monitor(db=db, monitor_input=monitor_input)
     return monitor 
+
+@router.get("/", response_model=list[MonitorResponse], summary="Get list of monitors")
+async def list_monitors(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
+    monitors = await get_monitors(db=db, skip=skip, limit=limit)
+    return monitors
+
+@router.get("/{monitor_id}", response_model=MonitorResponse, summary="Get a specific monitor by ID")
+async def read_monitor(monitor_id: int, db: AsyncSession = Depends(get_db)):
+    monitor = await get_monitor_by_id(db=db, monitor_id=monitor_id)
+    if not monitor:
+        raise HTTPException(status_code=404, detail="Monitor not found")
+    return monitor
+
+@router.delete("/{monitor_id}", summary="Delete a monitor by ID")
+async def remove_monitor(monitor_id: int, db: AsyncSession = Depends(get_db)):
+    monitor = await delete_monitor(db=db, monitor_id=monitor_id)
+
+    if not monitor:
+        raise HTTPException(status_code=404, detail="Monitor not found")
+    return {"message": "Monitor successfully deleted", "id": monitor_id}
--- app/db/crud.py
+++ app/db/crud.py
@@ -1,4 +1,6 @@
+from pexpect import spawnbase
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from app.db.models import Monitor
 from app.schemas.monitor import MonitorCreate
 
@@ -15,3 +17,20 @@ async def create_monitor(db: AsyncSession, monitor_input: MonitorCreate):
     await db.refresh(db_monitor)
     return db_monitor
 
+async def get_monitors(db: AsyncSession, skip: int = 0, limit: int = 100):
+    stmt = select(Monitor).offset(skip).limit(limit)
+    result = await db.execute(stmt)
+    return result.scalars().all()
+
+async def get_monitor_by_id(db: AsyncSession, monitor_id: int):
+    stmt = select(Monitor).where(Monitor.id == monitor_id)
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+
+async def delete_monitor(db: AsyncSession, monitor_id: int):
+    monitor = await get_monitor_by_id(db=db, monitor_id=monitor_id)
+    if not monitor:
+        return None
+    await db.delete(monitor)
+    await db.commit()
+    return monitor
